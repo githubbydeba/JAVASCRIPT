c786ba2ae9841b988d0d4b5cb9394c0f4c7e40bb
Debajyoti Pradhan <105050817+githubbydeba@users.noreply.github.com>
2023-09-28 17:57:24 +0000

    con

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,22 @@
+let score = undefined
+
+// console.log(typeof score);
+
+let valueinNumber = Number(score)
+// console.log(typeof(valueinNumber));
+// console.log(valueinNumber)
+
+// "33"=>33
+//"33abs"=> NaN
+
+let isLoggedIn = ""
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn);
+
+//1=>true 0=>false
+//"""=>false
+//"deba"=>true
+
+let someNumber =33
+let StringNumber = String(someNumber)
+console.log(StringNumber);
