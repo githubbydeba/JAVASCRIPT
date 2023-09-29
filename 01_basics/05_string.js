b7b4393503d1bd975d1dabfd06a3c668288b7e83
Debajyoti Pradhan <105050817+githubbydeba@users.noreply.github.com>
2023-09-29 16:35:54 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,23 @@
+const name ="debajyoti"
+const repoCount = 50
+
+// console.log(name + repoCount);
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName= new String ('deba-jyoti')
+// console.log(gameName[0]);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('t'));
+const newString =gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newString1 = "   debajyoti  "
+console.log(newString1);
+console.log(newString1.trim());
+
