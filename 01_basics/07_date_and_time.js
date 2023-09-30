c14c2e0dfcb52c5c1956cda200c857ab4a9e89dc
Debajyoti Pradhan <105050817+githubbydeba@users.noreply.github.com>
2023-09-30 14:33:44 +0000

    date&time

---
+++ 01_basics/07_date_and_time.js
@@ -0,0 +1,6 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
